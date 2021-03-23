a4145c83edc3a89d1b2116b4aad9a0784168278a
Charlie Welch <cfwelch@umich.edu>
2021-03-23 17:24:34 -0400

    added random length timer option and pydub for sound playing, removed osx support

--- timer.py
+++ timer.py
@@ -1,15 +1,20 @@
 
 
-import psutil, time, os
+import random, time, os
 
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 from argparse import ArgumentParser
 
+timeup = AudioSegment.from_mp3("ting.mp3")
+
 #dir_path = os.path.dirname(os.path.realpath(__file__))
-dir_path = "~/workspace/data/icons"
+dir_path = "."
 
 icon1_loc = dir_path + "/hourglass.png"
 icon2_loc = dir_path + "/hourglass3.png"
+EXPIRE = 5*1000
+LINUX_URGENCY = "" #--urgency=critical
 
 def remain(num_min):
     hours = int(num_min/60)
@@ -17,33 +22,40 @@ def remain(num_min):
     alert_str = str(mins) + " minutes remain..."
     if hours > 0:
         alert_str = str(hours) + " hours and " + alert_str
-    if psutil.OSX:
-        os.system("osascript -e 'display notification \"" + alert_str + "\" with title \"{}\"'")
-    elif psutil.LINUX:
-        os.system("notify-send --urgency=critical -i " + icon1_loc + " '" + alert_str + "'")
-    elif psutil.WINDOWS:
-        print("Windows is not supported...")
+    os.system("notify-send " + LINUX_URGENCY + " -i " + icon1_loc + " '" + alert_str + "' -t " + str(EXPIRE))
+
+def start():
+    os.system("notify-send " + LINUX_URGENCY + " -i " + icon1_loc + " 'Starting!' -t " + str(EXPIRE))
 
 def done():
-    if psutil.OSX:
-        os.system("osascript -e 'display notification \"Time is up!\" with title \"{}\"'")
-    elif psutil.LINUX:
-        os.system("notify-send --urgency=critical -i " + icon2_loc + " 'Time is up!'")
-    elif psutil.WINDOWS:
-        print("Windows is not supported...")
+    os.system("notify-send " + LINUX_URGENCY + " -i " + icon2_loc + " 'Time is up!' -t " + str(EXPIRE))
 
 # Parse arguments
 parser = ArgumentParser()
 parser.add_argument("-m", "--minutes", dest="minutes", help="Number of minutes to wait for", default=30, type=int)
 parser.add_argument("-aa", "--alert-at", dest="alert_at", help="Minute interval of alerts", default=5, type=int)
+parser.add_argument("-r", "--random", dest="random", help="Randomly vary the amount of time between alarms", action="store_true", default=False)
+parser.add_argument("-ra", "--range", dest="rrange", help="Sets the range of random to minutes +- this value", default=3, type=int)
+parser.add_argument("-l", "--loop", dest="loop", help="Set a new timer when this one expires", default=False, action="store_true")
 opt = parser.parse_args()
 
-rem_time_min = opt.minutes
-while rem_time_min > 0:
-    remain(rem_time_min)
-    sleep_interval = min(rem_time_min, opt.alert_at)
-    time.sleep(sleep_interval*60)
-    rem_time_min -= sleep_interval
+while(True):
+    rem_time_min = opt.minutes
+    if opt.random:
+        rem_time_min = rem_time_min - opt.rrange + random.random() * 2 * opt.rrange
 
-playsound('ting.mp3')
-done()
+    while rem_time_min > 0:
+        if not opt.random:
+            remain(rem_time_min)
+        else:
+            remain(rem_time_min)
+            # start()
+        sleep_interval = min(rem_time_min, opt.alert_at)
+        time.sleep(sleep_interval*60)
+        rem_time_min -= sleep_interval
+
+    play(timeup)
+    done()
+
+    if not opt.loop:
+        break
